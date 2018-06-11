cb897d34037700a5ca5fb8a279b7c5c575dbee25
LGY3030 <lgy3030@gmail.com>
2018-06-11 17:53:21 +0800

    Update
    build simple socket program of server.c and client.c

--- server.c
+++ server.c
@@ -1,34 +1,40 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-int main(void){
-    
-    //socket creation
-    int sockfd = 0;
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);//create socket
-    if(sockfd == -1){
-        printf("Fail to create a socket");
+int main(int argc , char *argv[])
+
+{
+    //socket的建立
+    char inputBuffer[256] = {};
+    char message[] = {"Hi,this is server.\n"};
+    int sockfd = 0,forClientSockfd = 0;
+    sockfd = socket(AF_INET , SOCK_STREAM , 0);
+
+    if (sockfd == -1){
+        printf("Fail to create a socket.");
     }
 
-    //socket connection
-    struct sockaddr_in info;
-    bzero(&info,sizeof(info));
-    info.sin_family = PF_INET;
-    info.sin_addr.s_addr = inet_addr("127.0.0.1");
-    info.sin_port = htons(8080);
-    int err = connect(sockfd,(struct sockaddr *)&info,sizeof(info));
-    if(err==-1){
-        print("Connection error");
-    }
+    //socket的連線
+    struct sockaddr_in serverInfo,clientInfo;
+    int addrlen = sizeof(clientInfo);
+    bzero(&serverInfo,sizeof(serverInfo));
 
+    serverInfo.sin_family = PF_INET;
+    serverInfo.sin_addr.s_addr = INADDR_ANY;
+    serverInfo.sin_port = htons(8700);
+    bind(sockfd,(struct sockaddr *)&serverInfo,sizeof(serverInfo));
+    listen(sockfd,5);
+
+    while(1){
+        forClientSockfd = accept(sockfd,(struct sockaddr*) &clientInfo, &addrlen);
+        send(forClientSockfd,message,sizeof(message),0);
+        recv(forClientSockfd,inputBuffer,sizeof(inputBuffer),0);
+        printf("Get:%s\n",inputBuffer);
+    }
     return 0;
-
-
-
-
 }
